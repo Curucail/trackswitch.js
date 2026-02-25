deacc7f6fb14b950fb333fcbcdfff9f783a705b9
Manuel Peters <manuel.peters2415@gmail.com>
2026-02-25 02:19:33 +0100

    remove jquery part 3

--- build/concat-css.js
+++ build/concat-css.js
@@ -2,8 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-// Read CSS files in order
-const reset = fs.readFileSync('dist/tmp/reset.css', 'utf8');
+// Read CSS source
 const trackswitch = fs.readFileSync('css/trackswitch.css', 'utf8');
 
 // Create dist directory if needed
@@ -12,6 +11,5 @@ if (!fs.existsSync(distDir)) {
   fs.mkdirSync(distDir, { recursive: true });
 }
 
-// Concatenate
-fs.writeFileSync('dist/tmp/concat.css', reset + '\n' + trackswitch);
-console.log('✓ Concatenated CSS');
+fs.writeFileSync('dist/tmp/concat.css', trackswitch);
+console.log('✓ Prepared CSS');
